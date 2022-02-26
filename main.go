e47366331dfbe2c78979a8563cd8963a3824af76
Mike Gleen <mike.gleen@gmail.com>
2022-02-26 17:26:02 +0000

    add flags

--- main.go
+++ main.go
@@ -1,29 +1,71 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
-	"os"
+
+	// "runtime/pprof"
 	"time"
 )
 
 const RAWBOARD = "data/rawboard.csv"
-const verbose = 1
+
+var verbose int
+
+type argstruct struct {
+	board    string
+	column   int
+	dijkstra bool
+	games    int
+	maxcost  int
+	nplayers int
+	print    bool
+	row      int
+	timeit   int
+	turns    int
+	verbose  int
+}
+
+func getargs() argstruct {
+	args := argstruct{}
+	flag.StringVar(&args.board, "board", RAWBOARD, `The file containing the board description.`)
+	flag.IntVar(&args.column, "column", 0, `Start column. For testing.`)
+	flag.BoolVar(&args.dijkstra, "dijkstra", false, "")
+	flag.IntVar(&args.games, "games", 1, `Number of games to play. Defaults is 1.`)
+	flag.IntVar(&args.maxcost, "maxcost", math.MaxInt, `Maximum distance of interest. For testing.`)
+	flag.IntVar(&args.nplayers, "nplayers", 4, `The number of players; the default is 4.`)
+	flag.BoolVar(&args.print, "print", false, "Print the finished board with distances.")
+	flag.IntVar(&args.row, "row", 0, `Start row. For testing.`)
+	flag.IntVar(&args.timeit, "timeit", 0, `Time the dijkstra function with this number of iterations.`)
+	flag.IntVar(&args.turns, "turns", math.MaxInt, `Stop the game after this number of turns.`)
+	flag.IntVar(&args.verbose, "verbose", 1, `Modify verbosity`)
+	flag.Parse()
+	return args
+}
 
 func main() {
-	// PqMain()
-	// os.Exit(0)
-	var rawboard [][]string
-	if len(os.Args) < 2 {
-		fmt.Printf("No parameters, using \"%v\"\n", RAWBOARD)
-	}
-	// rawboard := ReadBoard(RAWBOARD, true)
-	// for _, row := range rawboard {
-	// 	fmt.Printf("%v\n", row)
+
+	// cpufile, err := os.Create("cpu.pprof")
+	// if err != nil {
+	// 	panic(err)
 	// }
-	rawboard = ReadBoard(RAWBOARD, false)
-	for _, row := range rawboard {
-		fmt.Printf("%v\n", row)
+	// err = pprof.StartCPUProfile(cpufile)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer cpufile.Close()
+	// defer pprof.StopCPUProfile()
+
+	var rawboard [][]string
+	args := getargs()
+	verbose = args.verbose
+	// fmt.Println("timeit", args.timeit)
+	rawboard = ReadBoard(args.board, false)
+	if verbose > 1 {
+		for _, row := range rawboard {
+			fmt.Printf("%v\n", row)
+		}
 	}
 
 	node12 := NewNode(1, 2, false)
@@ -44,14 +86,21 @@ func main() {
 		g.PrintBoard()
 	}
 
-	iterations := 10000
+	iterations := args.timeit
 	start := time.Now().UnixMilli()
+	for n := 0; n < iterations; n++ {
+		g.ResetGraph()
+	}
+	end := time.Now().UnixMilli()
+	elapsed := end - start
+	fmt.Printf("reset time: %v\n", elapsed)
+	start = time.Now().UnixMilli()
 	for n := 0; n < iterations; n++ {
 		g.ResetGraph()
 		_, _ = dijkstra(g, g.Nodes[0], math.MaxInt, verbose)
 	}
-	end := time.Now().UnixMilli()
-	elapsed := end - start
+	end = time.Now().UnixMilli()
+	elapsed = end - start
 	fmt.Printf("elapsed: %v\n", elapsed)
-	fmt.Printf("Time per iteration: %v ms", float64(elapsed)/float64(iterations))
+	fmt.Printf("Time per iteration: %v ms\n", float64(elapsed)/float64(iterations))
 }
